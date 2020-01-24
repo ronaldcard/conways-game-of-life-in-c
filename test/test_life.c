17bcef1ec3ba753aca1f7c43bc22e6d8cdeca262
Tony Card <tony.card@accenture.com>
2020-01-24 09:45:01 -0500

    updated to fail the first test

--- test/test_life.c
+++ test/test_life.c
@@ -8,7 +8,12 @@ TEST_SETUP(Life){}
 
 TEST_TEAR_DOWN(Life){}
 
+TEST(Life, modify_this_to_begin)
+{
+    TEST_FAIL_MESSAGE("Failing on purpose!!!");
+}
+
 TEST_GROUP_RUNNER(Life)
 {
-    
+    RUN_TEST_CASE(Life, modify_this_to_begin);
 }
